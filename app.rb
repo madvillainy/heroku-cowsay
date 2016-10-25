101121b566436ac2e009ac8a785d1642f0fbe7fe
Patrick J. Orlando <patrickorlando93@gmail.com>
2016-10-25 14:07:52 +1100

    cowsay

--- app.rb
+++ app.rb
@@ -16,5 +16,5 @@ require 'fortune_gem'
 
 get '/' do
   # "Hello, world"
-  Cow.new.say FortuneGem.give_fortune({:max_length => 80})
+  sprintf Cow.new.say FortuneGem.give_fortune({:max_length => 80})
 end
