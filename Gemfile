0dd4b3eaa7deafee4e9a3bd43832eac53e648ba2
Patrick J. Orlando <patrickorlando93@gmail.com>
2016-10-25 14:00:19 +1100

    sinatra

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
-ruby "2.0.0"
+ruby "2.3.1"
 
 gem 'ruby_cowsay'
 gem 'fortune_gem'
+gem 'sinatra', '1.1.0'
--- app.rb
+++ app.rb
@@ -1,14 +1,19 @@
-require 'socket'
+# require 'socket'
+require 'sinatra'
 require 'ruby_cowsay'
 require 'fortune_gem'
 
 server = TCPServer.new('0.0.0.0', 80)
 
-loop do
-  socket = server.accept
-  request = socket.gets
-  response = Cow.new.say FortuneGem.give_fortune({:max_length => 80})
-  socket.print "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n" + "Content-Length: #{response.bytesize}\r\nConnection: close\r\n\r\n"
-  socket.print response
-  socket.close
+# loop do
+#   socket = server.accept
+#   request = socket.gets
+#   response = Cow.new.say FortuneGem.give_fortune({:max_length => 80})
+#   socket.print "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n" + "Content-Length: #{response.bytesize}\r\nConnection: close\r\n\r\n"
+#   socket.print response
+#   socket.close
+# end
+
+get '/' do
+  "Hello, world"
 end
